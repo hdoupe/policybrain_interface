34d665d5b77acfb13dc84ec547e7050508eca243
Henry Doupe <hdoupe@yahoo.com>
2018-05-16 14:51:12 -0400

    Try another format...

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,10 +31,20 @@
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
+# extensions = [
+#     'sphinx.ext.autodoc',
+#     'sphinx.ext.napoleon',
+#     'sphinx.ext.viewcode',
+# ]
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.napoleon',
+    'sphinx.ext.todo',
+    'sphinx.ext.ifconfig',
     'sphinx.ext.viewcode',
+    'sphinx.ext.autosummary',
+    'sphinx.ext.extlinks',
+    'sphinx.ext.intersphinx',
+    'numpydoc',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
